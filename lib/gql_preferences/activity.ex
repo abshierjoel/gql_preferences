4d032a85869dd2233317d4271dbbd1cca16baf9f
Joel Abshier <jabshier@midwayusa.com>
2021-10-04 14:08:50 -0500

    Add `get` to Activity monitor

--- lib/gql_preferences/activity.ex
+++ lib/gql_preferences/activity.ex
@@ -1,7 +1,7 @@
-defmodule Activity do
+defmodule ActivityServer do
   use GenServer
 
-  def start_link(_), do: GenServer.start_link(__MODULE__, %{})
+  def start_link(_), do: GenServer.start_link(__MODULE__, %{}, name: ActivityServer)
   def init(_), do: {:ok, %{}}
 
   def handle_cast({:update, key}, state) do
@@ -11,6 +11,16 @@ defmodule Activity do
 
       _ ->
         {:noreply, Map.put(state, key, 1)}
+    end
+  end
+
+  def handle_call({:get, key}, state) do
+    case Map.has_key?(state, key) do
+      true ->
+        {:reply, {:ok, Map.get(state, key)}, state}
+
+      _ ->
+        {:reply, {:error, "Requested "}, state}
     end
   end
 end
--- lib/gql_preferences/activity_monitor.ex
+++ lib/gql_preferences/activity_monitor.ex
@@ -1,4 +1,5 @@
 defmodule ActivityMonitor do
-  def update_resolver_activity(key) do
-  end
+  @activity_server GenServer.whereis(ActivityServer)
+  def update_resolver_activity(key), do: @activity_server |> GenServer.cast({:update, key})
+  def fetch_resolver_activity(key), do: @activity_server |> GenServer.call({:get, key})
 end
